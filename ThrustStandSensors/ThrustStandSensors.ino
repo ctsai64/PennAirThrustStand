351e0296b4d801b67d15ea803c4d8d1bd1d165e8
ctsai64 <168315570+ctsai64@users.noreply.github.com>
2025-10-22 20:37:46 -0400

    Working with loadcell, temp, rpm

--- ThrustStandSensors/ThrustStandSensors.ino
+++ ThrustStandSensors/ThrustStandSensors.ino
@@ -1,5 +1,4 @@
-// Load cell, temperture sensor
-
+// Load cell, temperature sensor, and RPM measurement
 #include <HX711_ADC.h>
 #include <DHT.h>
 
@@ -8,124 +7,130 @@ const int HX711_sck = 1;  // MCU > HX711 sck pin
 const int DHTPIN = 4;
 const int hall = 5;
 
-unsigned long t = 0;
-float temp;
-int hallVal;
+volatile unsigned long pulseCount = 0;
+unsigned long lastTime = 0;
+float rpm = 0;
 
+const int magnets = 8;                // Number of magnets per revolution
+const unsigned long interval = 1000;  // Interval to calculate RPM (ms)
+
+float temp;
+unsigned long t = 0;
 
 DHT dht(DHTPIN, DHT11);
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
+void countPulse() {
+  pulseCount++;
+}
 
 void setup() {
-  pinMode(hall, INPUT);
   Serial.begin(9600);
-  delay(10);
-  Serial.println();
-  Serial.println("Starting...");
+  pinMode(hall, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(hall), countPulse, FALLING);
 
   dht.begin();
-  //temp = dht.readTemperature();
-  //Serial.print("Temperature: ");
-  //Serial.println(temp);
-
-
   LoadCell.begin();
-  // LoadCell.setReverseOutput(); // Uncomment to invert the output sign
+
   unsigned long stabilizingtime = 2000;
-  bool _tare = true; // Perform tare at startup
+  bool _tare = true;
+  delay(10);
 
-
+  Serial.println("\nStarting...");
   LoadCell.start(stabilizingtime, _tare);
+
   if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    Serial.println("Timeout, check HX711 wiring!");
     while (1);
   } else {
-    LoadCell.setCalFactor(1.0); // Default calibration value
+    LoadCell.setCalFactor(1.0); // Default calibration
     Serial.println("Startup complete");
   }
 
-
   while (!LoadCell.update());
-  calibrate(); // Start calibration procedure
+  calibrate(); // Start calibration
 }
-
 
 void loop() {
   static bool newDataReady = false;
-  const int serialPrintInterval = 0;
+  const unsigned long serialPrintInterval = 1000; // ms between data prints
 
-
+  // Update load cell
   if (LoadCell.update()) newDataReady = true;
 
+  // Print data if ready and interval elapsed
+  if (newDataReady && millis() - t > serialPrintInterval) {
+    newDataReady = false;
+    t = millis();
 
-  if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData();
-      Serial.print("Load cell output val: ");
-      Serial.println(i);
-      dht.begin();
-      temp = dht.readTemperature();
+    Serial.println();
+
+    // Load cell
+    float i = LoadCell.getData();
+    Serial.print("Load cell: ");
+    Serial.println(i);
+
+    // Temperature
+    temp = dht.readTemperature();
+    if (!isnan(temp)) {
       Serial.print("Temperature: ");
       Serial.println(temp);
-      hallVal = digitalRead(hall);
-      if(val == HIGH){
-        Serial.println(val);
-      }
-      newDataReady = false;
-      t = millis();
+    }
+
+    // RPM
+    unsigned long currentTime = millis();
+    if (currentTime - lastTime >= interval) {
+      noInterrupts();
+      unsigned long count = pulseCount;
+      pulseCount = 0;
+      interrupts();
+
+      rpm = (count / (float)magnets) * (60000.0 / interval);
+      Serial.print("RPM: ");
+      Serial.println(rpm, 1);
+
+      lastTime = currentTime;
     }
   }
-
 
   // Serial commands
   if (Serial.available() > 0) {
     char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();   // Tare
-    else if (inByte == 'r') calibrate();         // Calibrate
-    else if (inByte == 'c') changeCalFactor();   // Manually change calibration factor
+    if (inByte == 't') LoadCell.tareNoDelay();
+    else if (inByte == 'r') calibrate();
+    else if (inByte == 'c') changeCalFactor();
   }
-
 
   if (LoadCell.getTareStatus()) {
     Serial.println("Tare complete");
   }
 }
 
+// ---------- Calibration Functions ---------- //
 
 void calibrate() {
   Serial.println("***");
-  Serial.println("Start calibration:");
-  Serial.println("Place the load cell on a level surface.");
-  Serial.println("Remove all weight from the load cell.");
-  Serial.println("Send 't' to tare.");
-
+  Serial.println("Start calibration: place load cell level and empty. Send 't' to tare.");
 
   bool _resume = false;
   while (!_resume) {
     LoadCell.update();
-    if (Serial.available() > 0) {
-      char inByte = Serial.read();
-      if (inByte == 't') LoadCell.tareNoDelay();
-    }
+    if (Serial.available() > 0 && Serial.read() == 't')
+      LoadCell.tareNoDelay();
     if (LoadCell.getTareStatus()) {
       Serial.println("Tare complete");
       _resume = true;
     }
   }
 
-
-  Serial.println("Place a known mass on the load cell.");
-  Serial.println("Then send the weight of this mass (e.g. 100.0) from Serial Monitor.");
-
-
+  Serial.println("Place known mass, then send its weight (e.g. 100.0).");
   float known_mass = 0;
   _resume = false;
   while (!_resume) {
     LoadCell.update();
     if (Serial.available() > 0) {
       known_mass = Serial.parseFloat();
-      if (known_mass != 0) {
+      if (known_mass > 0) {
         Serial.print("Known mass: ");
         Serial.println(known_mass);
         _resume = true;
@@ -133,38 +138,31 @@ void calibrate() {
     }
   }
 
-
   LoadCell.refreshDataSet();
   float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
-
-
   Serial.print("New calibration value: ");
   Serial.println(newCalibrationValue);
-  Serial.println("Use this value as LoadCell.setCalFactor() in your sketch.");
-  Serial.println("***");
-  Serial.println("To re-calibrate, send 'r' from Serial Monitor.");
-  Serial.println("To manually edit calibration, send 'c'.");
-  Serial.println("***");
-}
+  LoadCell.setCalFactor(newCalibrationValue);
 
+  Serial.println("*** Calibration complete ***");
+}
 
 void changeCalFactor() {
   float oldCal = LoadCell.getCalFactor();
   Serial.println("***");
   Serial.print("Current calibration value: ");
   Serial.println(oldCal);
-  Serial.println("Send new value from Serial Monitor (e.g. 696.0)");
-
+  Serial.println("Send new value (e.g. 696.0):");
 
   float newCal = 0;
   bool _resume = false;
   while (!_resume) {
     if (Serial.available() > 0) {
       newCal = Serial.parseFloat();
-      if (newCal != 0) {
+      if (newCal > 0) {
+        LoadCell.setCalFactor(newCal);
         Serial.print("New calibration value: ");
         Serial.println(newCal);
-        LoadCell.setCalFactor(newCal);
         _resume = true;
       }
     }
@@ -172,4 +170,3 @@ void changeCalFactor() {
   Serial.println("Calibration factor updated.");
   Serial.println("***");
 }
-
